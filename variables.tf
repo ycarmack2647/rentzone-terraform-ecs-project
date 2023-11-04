5fc39765d49c79610ca627724adb3e1f076cc28a
Yashon <114156150+ycarmack2647@users.noreply.github.com>
2023-11-04 17:13:07 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+# environment variables
+variable "region" {
+  description = "region to create resources"
+  type = string
+}
+
+variable "project_name" {
+  description = "project name"
+  type = string
+}
+
+variable "environment" {
+  description = "environment"
+  type = string
+}
