31b937d3a7c4d341a730a793a4c7e02f890f0e45
Yashon Carmack <yashoncarmack@gmail.com>
2023-11-06 11:19:19 -0500

    created vpc file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,45 @@
-# environment variables
-variable "region" {
-  description = "region to create resources"
+# vpc variable
+variable "vpc_cider" {
+  default = "10.0.0.0/16"
+  description = "value of vpc cider"
   type = string
 }
 
-variable "project_name" {
-  description = "project name"
+
+variable "public_subnet_az1_cidr" {
+  default = "10.0.0.0/24"
+  description = "public subnet az1 cider"
   type = string
 }
 
-variable "environment" {
-  description = "environment"
+variable "public_subnet_az2_cidr" {
+  default = "10.0.1.0/24"
+  description = "public subnet az2 cider"
   type = string
 }
+variable "private_app_subnet_az1_cidr" {
+  default = "10.0.2.0/24"
+  description = "private app subnet az1 cider"
+  type = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  default = "10.0.3.0/24"
+  description = "private app subnet az2 cider"
+  type = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  default = "10.0.4.0/24"
+  description = "private data subnet az1 cider"
+  type = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  default = "10.0.5.0/24"
+  description = "private data subnet az2 cider"
+  type = string
+}
+
+
+
